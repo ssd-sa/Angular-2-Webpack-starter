1f6c5db3b24ed36ed83c092488deef08dc7d13cf
Ayman Gado <ayman@ssd.sa>
2016-09-26 15:08:02 +0200

    fix

--- src/vendor.ts
+++ src/vendor.ts
@@ -9,4 +9,4 @@ import '@angular/router';
 import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
-import './assets/scss/styles.scss'
+import './assets/scss/styles.scss';
